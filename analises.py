6ab55da0dd53aeff918ef43fb29cd9b81e3744d2
lucasptcastro <lucasptcastro@gmail.com>
2023-04-25 21:57:05 -0300

    🦄 refactor: refactor df variable

--- analises.py
+++ analises.py
@@ -3,10 +3,8 @@ import matplotlib.pyplot as plt
 
 class Analises:
     def __init__(self, dataframe, limitador=0) -> None:
-        self.df = pd.read_csv(dataframe, sep=";", encoding="ISO-8859-1")
+        self.df = pd.read_csv(dataframe, sep=";", encoding="ISO-8859-1") if limitador == 0 else pd.read_csv(dataframe, sep=";", encoding="ISO-8859-1")[0:limitador]
 
-        if limitador != 0: self.df = self.df[0:limitador]
-        pass
 
     def coletar_coluna(self, coluna):
         return self.df[f'{coluna}']
